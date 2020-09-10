963f3d660df589c221d5fefe2ee792bea3c9780d
Cristi Gutierrez <cristigtzname@gmail.com>
2020-09-09 21:37:54 -0500

    Finished JS validation

--- main.js
+++ main.js
@@ -1,16 +1,62 @@
 const form = document.getElementById('form');
 
-const firstname = document.getElementById('fname');
-const lastname = document.getElementById('lname');
-const email = document.getElementById('email');
-const password = document.getElementById('pssw');
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value === null) {
-        messages.push('First Name cannot be empty')
+    const firstName = form['fname'].value;
+    const lastName = form['lname'].value;
+    const email = form['email'].value;
+    const password = form['pssw'].value;
+
+
+
+
+    if (firstName === '') {
+        addErrorTo('fname', 'First Name cannot be empty');
+    } else {
+        removeErrorFrom('fname');
     }
 
+    if (lastName === '') {
+        addErrorTo('lname', 'Last Name cannot be empty');
+    } else {
+        removeErrorFrom('lname');
+    }
 
-    e.preventDefault()
-})
+    if (email === '') {
+        addErrorTo('email', 'Email is required');
+    } else if (!isValid(email)) {
+        addErrorTo('email', 'Looks like this is not an email');
+    } else {
+        removeErrorFrom('email');
+    }
+
+    if (password === '') {
+        addErrorTo('pssw', 'Password cannot be empty');
+    } else {
+        removeErrorFrom('pssw');
+    }
+});
+
+function addErrorTo(field, message) {
+    const formControl = form[field].parentNode;
+    formControl.classList.add('error');
+
+    const small = form[field].parentNode.querySelector('small');
+    small.innerText = message;
+    small.style.opacity = '1';
+}
+
+function removeErrorFrom(field) {
+    const formControl = form[field].parentNode;
+    formControl.classList.remove('error');
+
+    const small = formControl.querySelector('small');
+    small.style.opacity = 0;
+}
+
+
+function isValid(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
